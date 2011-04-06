63a5f86d3aeab02cbb4fd2f4ff963a1608921d66
mageshb <mageshb@thoughtworks.com>
2011-04-06 17:18:50 +0530

    Moved performance test to differnt solution and created new target for that.

---
+++ Src/AnujBank/PerformanceTest/PerformanceTest.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AnujBank;
+using Moq;
+using NUnit.Framework;
+using TestAnujBank;
+
+namespace PerformanceTest
+{
+    [TestFixture]
+    public class PerformanceTest
+    {
+        [Test]
+        public void PerformanceTestForStructureProcessing()
+        {
+            var repositoryStub = new RepositoryStub();
+            var interestRate = new Mock<InterestRates>();
+            interestRate.Setup(ir => ir.PositiveInterestRate()).Returns(2.0);
+            interestRate.Setup(ir => ir.NegativeInterestRate()).Returns(3.0);
+            var feedProcessor = new FeedProcessor(repositoryStub, "../../../Pickup/testdata/feed.csv");
+            feedProcessor.Process();
+
+            var paymentInstructionService = new PaymentInstructionService();
+            var structures = repositoryStub.Accounts.GroupBy(a => a.GetClientId())
+                                            .Select(g => new
+                                                             {
+                                                                 ClientAccount = new ClientAccounts(g), 
+                                                                 Allocation=GetAllocation(g.ToList())
+                                                             })
+                                            .Select(cs=>new Structure(cs.ClientAccount, cs.Allocation, interestRate.Object, paymentInstructionService));
+            var ss = structures.ToList();
+
+            var start = DateTime.Now.Ticks;
+            ss.ForEach(s=>s.GeneratePaymentInstruction());
+            var stop = DateTime.Now.Ticks;
+            Console.WriteLine((stop-start)/10000);
+        }
+
+        private List<Allocation> GetAllocation(List<Account> accounts)
+        {
+            var accPercent = 100/accounts.Count;
+            var leftOverPercent = 100 - accPercent*(accounts.Count - 1);
+            var allocations = new List<Allocation>();
+            for (int i = 0; i < accounts.Count-1; i++)
+            {
+                allocations.Add(new Allocation(accounts[i], accPercent));
+            }
+            allocations.Add(new Allocation(accounts[accounts.Count-1], leftOverPercent));
+            return allocations;
+        }
+    }
+
+    public class RepositoryStub:IRepository
+    {
+        public readonly List<Account> Accounts = new List<Account>();
+        public void Save(Account account)
+        {
+            Accounts.Add(account);
+        }
+    }
+}
